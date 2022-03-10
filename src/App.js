b1196cba7814adf7b9fa6464e19474e5a1a1a8f5
Miguel Lima <miguellimaoliver97@gmail.com>
2022-03-10 17:31:15 -0300

    Requisito 3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import SearchBar from './components/SearchBar';
+import ShoppingCard from './components/ShoppingCard';
 import Input from './components/Input';
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <Input />
-      </div>
+      <>
+        <div>
+          <BrowserRouter>
+            <Switch>
+              <Route exact path="/" component={ SearchBar } />
+              <Route path="/shoppingcard" component={ ShoppingCard } />
+            </Switch>
+          </BrowserRouter>
+        </div>
+        <div className="App">
+          <Input />
+        </div>
+      </>
+
     );
   }
 }
