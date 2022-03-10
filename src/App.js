ae37e5818a3e230f2b1a99f91bd7265a0331d51e
Elienay-Alves <elienay8486@gmail.com>
2022-03-10 17:54:06 -0300

    Requisito 4 Feito. Vai mano, nunca te pedi nada

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Input from './components/Input';
+import Category from './components/Categories';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <Input />
+        <Category />
       </div>
     );
   }
---
+++ src/components/Categories.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import * as api from '../services/api';
+
+class Category extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      categories: [],
+    };
+  }
+
+  componentDidMount() {
+    this.fetchCategory();
+  }
+
+  fetchCategory = async () => {
+    const requisition = await api.getCategories();
+    this.setState({
+      categories: [...requisition],
+    });
+  }
+
+  render() {
+    const { categories } = this.state;
+
+    return (
+      <aside>
+        <ul>
+          Categorias:
+          {
+            categories.map((category) => (
+              <li key={ category.id } data-testid="category">
+                {category.name}
+              </li>))
+          }
+        </ul>
+      </aside>
+    );
+  }
+}
+
+export default Category;
