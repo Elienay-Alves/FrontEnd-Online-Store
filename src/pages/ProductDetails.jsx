491e8068618d83ae61e4df7d28c6f0ad0653a246
GugaSantos <guga.santos.guga@gmail.com>
2022-03-11 17:59:02 -0300

    Requisito 09 ok

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom';
 import { getProductFromID } from '../services/api';
+import Button from '../components/Button';
+
+const TIMEOUT = 500;
 
 class ProductDetails extends Component {
   constructor() {
@@ -20,16 +24,40 @@ componentDidMount = async () => {
   const response = await getProductFromID(path);
   this.setState({
     productInfos: response,
+    clicked: false,
+    cartItems: [],
   });
 
 //   console.log(path);
 //   console.log(await getProductFromID(path));
 }
 
+handleClick = async () => {
+  const { cartItems } = this.state;
+  const { location: { pathname } } = this.props;
+  const path = pathname.split('/')[2];
+  const response = await getProductFromID(path);
+  this.setState({
+    productInfos: response,
+  });
+  setTimeout(() => {
+    const savedItems = localStorage.getItem('productCart');
+    const parseItem = JSON.parse(savedItems);
+    this.setState({
+      cartItems: parseItem,
+    });
+  }, TIMEOUT);
+  localStorage.setItem('productCart', JSON.stringify([...cartItems, response]));
+  this.setState({
+    clicked: true,
+  });
+}
+
 render() {
-  const { productInfos } = this.state;
+  const { productInfos, clicked } = this.state;
   return (
     <div className="product-container">
+      <Button />
       <div className="product-img-container">
         <img
           src={ productInfos.thumbnail }
@@ -51,7 +79,16 @@ render() {
         >
           {`Quantidade disponível: ${productInfos.available_quantity}`}
         </h3>
+        <button
+          type="button"
+          data-testid="product-detail-add-to-cart"
+          onClick={ this.handleClick }
+        >
+          Adicionar ao carrinho
+
+        </button>
       </div>
+      { clicked ? <Redirect to="/shoppingcard" /> : null }
     </div>
   );
 }
